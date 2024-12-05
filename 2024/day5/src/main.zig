0fc5ba7168f0bda8d3e21c619479cb0aa6539e5a
Francesco Michele Barranca <francescomich99@gmail.com>
2024-12-05 12:39:11 -0500

    finished day5 part 2

--- 2024/day5/src/main.zig
+++ 2024/day5/src/main.zig
@@ -62,14 +62,64 @@ pub fn main() !void {
 
     std.debug.print("RULES LEN: {} --- chapters LEN {}\n", .{ rules.items.len, chapters.items.len });
 
-    try part1(&chapters, &rules);
+    var problematic_chapters = try part1(&chapters, &rules);
+
+    try part2(&problematic_chapters, &rules);
+}
+
+fn part2(problematic_chapters: *std.ArrayList(std.ArrayList(u32)), rules: *std.ArrayList(Rule())) !void {
+    
+    std.debug.print("\nPROBLEMATIC CHAPTERS Chapters LEN {}\n", .{ problematic_chapters.items.len });
+
+    var sum_of_middle_elements: u32 = 0;
+
+    while(true) {
+        var fixes_made = false;
+
+        for (problematic_chapters.items, 0..) |chapter, i| {
+            for(chapter.items, 0..) |page, current_page_index| {
+                for (0..chapter.items.len) |index| {
+                    for (rules.items) |rule| {
+                        const next_element_index = index + current_page_index + 1;
+                        if(next_element_index < chapter.items.len) {
+                            if(rule.isRuleBroken(page, chapter.items[next_element_index])) {
+                                std.debug.print("FIXING CHAPTER {} CAUSE: <RULE BROKEN -> {}|{} AGAINST {}--{}> CURRENT STATE {any}", .{ i + 1, rule.left, rule.right, page, chapter.items[next_element_index], chapter.items});
+                                
+                                const tmp = chapter.items[current_page_index];
+                                chapter.items[current_page_index] = chapter.items[next_element_index];
+                                chapter.items[next_element_index] = tmp;
+
+                                std.debug.print(" FIXED TO -> {any}\n\n", .{ chapter.items });
+
+                                fixes_made = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if(!fixes_made) break;
+    }
+
+    
+
+    for (problematic_chapters.items) |chapter| {
+        const middle_index = chapter.items.len / 2;
+
+        sum_of_middle_elements += chapter.items[middle_index];
+    }
+
+    std.debug.print("\n\n< PART 2 > SUM OF MIDDLE PROBLEMATIC CHAPTERS: {}\n", .{ sum_of_middle_elements });
 }
 
 
-fn part1(chapters: *std.ArrayList(std.ArrayList(u32)), rules: *std.ArrayList(Rule())) !void {
+fn part1(chapters: *std.ArrayList(std.ArrayList(u32)), rules: *std.ArrayList(Rule())) !std.ArrayList(std.ArrayList(u32)) {
     const allocator = std.heap.page_allocator;
     var approved_chapters = std.ArrayList(std.ArrayList(u32)).init(allocator);
     defer approved_chapters.deinit();
+    var problematic_chapters = std.ArrayList(std.ArrayList(u32)).init(allocator);
+    defer problematic_chapters.deinit();
 
     for (chapters.items, 0..) |chapter, i| {
         var approved = true;
@@ -82,6 +132,7 @@ fn part1(chapters: *std.ArrayList(std.ArrayList(u32)), rules: *std.ArrayList(Rul
                         //std.debug.print("CHECKING RULE: {}|{} AGAINST ELEMETS: {}--{}\n", .{ rule.left, rule.right,  page, chapter.items[next_element_index]});
                         if(rule.isRuleBroken(page, chapter.items[next_element_index])) {
                             std.debug.print("CHAPTER {} INVALIDATED <RULE BROKEN -> {}|{} AGAINST {}--{}>\n\n", .{ i + 1, rule.left, rule.right, page, chapter.items[next_element_index]});
+                            try problematic_chapters.append(chapter);
                             approved = false;
                             break :outer;
                         }
@@ -106,4 +157,6 @@ fn part1(chapters: *std.ArrayList(std.ArrayList(u32)), rules: *std.ArrayList(Rul
     }
 
     std.debug.print("\n\n< PART 1 > SUM OF MIDDLE APPROVED CHAPTERS: {}\n", .{ sum_of_middle_elements });
+
+    return problematic_chapters.clone();
 }
